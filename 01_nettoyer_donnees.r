c6390b5dcfc6723f235e7a971f6a95b54e897971
targ0401 <targ0401@usherbrooke.ca>
2023-03-23 17:47:54 -0400

    ajout code SQL

--- 01_nettoyer_donnees.r
+++ 01_nettoyer_donnees.r
@@ -174,3 +174,52 @@ collaboration[2594,4]="E2022"#changement de E2023 en E2022
 collaboration[2648,4]="E2022"#changement de E2023 en E2022
 collaboration<-collaboration[-c(2504),]#sup cas avec des NA pour les cours et annee
 collaboration<-collaboration[-c(2504),]#sup cas avec des NA pour les cours et annee
+
+
+#SQL####
+library(RSQLite)
+con <- dbConnect(SQLite(), dbname="~/BAC/session 4/BIO500/travail/BIO500/projet1.db")
+#creation table SQL####
+dbSendQuery(con,etudiant_sql <- "
+CREATE TABLE etudiant_tb(
+    prenom_nom VARCHAR (40),
+    prenom VARCHAR(40),
+    nom VARCHAR(40),
+    region_administrative VARCHAR(40),
+    regime_coop BOLEAN,	
+    formation_prealable VARCHAR(40),
+    annee_debut  VARCHAR(20),
+    programme VARCHAR(20),	
+    PRIMARY KEY (prenom_nom)
+);")
+dbSendQuery(con, etudiant_sql)
+dbListTables(con)
+
+dbSendQuery(con, collaborations_sql <- '
+CREATE TABLE collaboration_tb(
+    etudiant1     VARCHAR(40),
+    etudiant2     VARCHAR(40),
+    sigle   VARCHAR(10),
+    session  VARCHAR(10),
+    PRIMARY KEY (etudiant1, etudiant2, sigle, session),
+    FOREIGN KEY (etudiant1) REFERENCES etudiant(prenom_nom),
+    FOREIGN KEY (etudiant2) REFERENCES etudiant(prenom_nom),
+    FOREIGN KEY (sigle) REFERENCES cours(sigle)
+); ')
+dbSendQuery(con, collaborations_sql)
+dbListTables(con)
+
+dbSendQuery(con,cours_sql <- '
+CREATE TABLE cours_tb (
+  sigle   VARCHAR(20),
+  optionnel       BOLEAN NOT NULL,
+  credit     VARCHAR(10),
+  PRIMARY KEY (sigle)
+); ')
+dbSendQuery(con, cours_sql)
+dbListTables(con)
+
+#####
+dbWriteTable(con, append = TRUE, name = "etudiant", value = etudiant, row.names = FALSE)
+dbWriteTable(con, append = TRUE, name = "collaboration", value = collaboration, row.names = FALSE)
+dbWriteTable(con, append = TRUE, name = "cour", value = cour, row.names = FALSE)
