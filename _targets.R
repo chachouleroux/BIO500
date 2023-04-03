d2859a3b585dc054748d9a5bfc105067c9aea436
targ0401 <targ0401@usherbrooke.ca>
2023-04-03 17:32:16 -0400

    creation targets

---
+++ _targets.R
@@ -0,0 +1,46 @@
+setwd()
+library(targets)
+library(tarchetypes)
+library(rmarkdown)
+library(tarchet)
+
+tar_option_set(packages = c("RSQLite","igraph","dplyr"))
+
+source("donnees.r")
+
+list(
+  tar_target(coll,
+                datacoll()
+),
+tar_target(cour_raw,
+           data_cours()
+           ),
+tar_target(cour, 
+           clean_cour(cour_raw)
+           ),
+tar_target(etudiant_raw,
+           data_etudiant()
+           ),
+tar_target(etudiant,
+           clean_etudiant(etudiant_raw)
+           ),
+tar_target(connectionSQL,
+           conSQL()
+           ),
+tar_target(SQL,
+           createSQL(etudiant,collaboration,cour,connectionSQL)
+           ),
+tar_target(nd_collab,
+           requete1()
+           ),
+tar_target(reseau_collab,
+           requete2()
+           ),
+tar_target(table_nbcollab,
+           read.csv()#lien
+           ),
+tar_target(table_reseaucollab,
+           read.csv()#lien
+             ),           
+)
+  
