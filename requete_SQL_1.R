b6c9be79488c42b5d4909f0c2e70f851418e72e5
GirardChloe <124721169+GirardChloe@users.noreply.github.com>
2023-04-03 18:40:21 -0400

    requête 1, pas finie

---
+++ requete_SQL_1.R
@@ -0,0 +1,9 @@
+nlien_sql<-"
+SELECT etudiant1,
+count(etudiant2) AS nlien
+FROM collaboration
+GROUP BY etudiant1
+ORDER BY nlien DESC;"
+nb_lien<-dbGetQuery(con,nlien_sql)
+nb_lien
+write.csv(nb_lien,file = "nb_lien.csv")
