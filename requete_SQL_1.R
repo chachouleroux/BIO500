ef163feab3fb1d7f1ea6f656ec9074f030de9d91
GirardChloe <124721169+GirardChloe@users.noreply.github.com>
2023-04-12 12:51:47 -0400

    Ajout de commentaires pour la requête 1.

--- requete_SQL_1.R
+++ requete_SQL_1.R
@@ -1,11 +1,11 @@
-requete1 <- function() {
+requete1 <- function() {    # Extraction de l'information sur les collaborations.
   nlien_sql<-"
-SELECT etudiant1,
-count(etudiant2) AS nlien
-FROM collaboration
-GROUP BY etudiant1
-ORDER BY nlien DESC;"
+SELECT etudiant1,           # Sélectionne la colonne etudiant1.
+count(etudiant2) AS nlien   # Renomme la colonne etudiant2 comme était nlien.
+FROM collaboration          # À partir de la table collaboration.
+GROUP BY etudiant1          # Données agrégées sur le champ etudiant1.
+ORDER BY nlien DESC;"       # Tri des données en ordre décroissant.
   nb_lien<-dbGetQuery(con,nlien_sql)
   nb_lien
-  write.csv(nb_lien,file = "nb_lien.csv")
+  write.csv(nb_lien,file = "nb_lien.csv") # Sauvegarde de la requête 1.
 }
