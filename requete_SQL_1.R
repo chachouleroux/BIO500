2a6d1f61677b64b9d3eb646d6d10b5db27f2867e
GirardChloe <124721169+GirardChloe@users.noreply.github.com>
2023-04-12 12:58:23 -0400

    Mise à jour commentaires.

--- requete_SQL_1.R
+++ requete_SQL_1.R
@@ -1,4 +1,4 @@
-requete1 <- function() {    # Extraction de l'information sur les collaborations.
+requete1 <- function() {    # Extraction de l'information sur le nombre de collaborations.
   nlien_sql<-"
 SELECT etudiant1,           # Sélectionne la colonne etudiant1.
 count(etudiant2) AS nlien   # Renomme la colonne etudiant2 comme était nlien.
@@ -7,5 +7,5 @@ GROUP BY etudiant1          # Données agrégées sur le champ etudiant1.
 ORDER BY nlien DESC;"       # Tri des données en ordre décroissant.
   nb_lien<-dbGetQuery(con,nlien_sql)
   nb_lien
-  write.csv(nb_lien,file = "nb_lien.csv") # Sauvegarde de la requête 1.
+  write.csv(nb_lien,file = "nb_lien.csv") # Sauvegarde de la requête SQL 1.
 }
