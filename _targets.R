f91578bc6c58ed963308f1e4968fc5efc80d1c54
targ0401 <targ0401@usherbrooke.ca>
2023-04-12 10:34:30 -0400

    changer les noms pour les relier a la bonne source

--- _targets.R
+++ _targets.R
@@ -9,18 +9,20 @@ tar_option_set(packages = c("RSQLite","igraph","dplyr"))
 source("donnees.r")
 source("clean_collab.r")
 source("clean_cour.r")
+source("clean_etudiant.r")
 source("conSQL.r")
 source("createSQL.r")
-source("requete1.r")
-source("requete2.r")
+source("requete_SQL_1.r")
+source("requete_SQL_2.r")
+source("connectance.r")
 
 
 list(
   tar_target(data,
-             donnees.r()#lien
+             donnees.r("~/BAC/session 4/BIO500/travail/BIO500")#lien
              ),
 tar_target(collaboration,
-           clean_collaboration()
+           clean_collab()
            ),
 tar_target(cour, 
            clean_cour()
@@ -35,19 +37,19 @@ tar_target(SQL,
            createSQL()
            ),
 tar_target(nd_collab,
-           requete1()
+           requete_SQL_1()
            ),
 tar_target(reseau_collab,
-           requete2()
+           requete_SQL_2()
            ),
 tar_target(post_traitement,
            connectance()
            ),
 tar_target(table_nbcollab,
-           read.csv()#lien
+           read.csv("~/BAC/session 4/BIO500/travail/BIO500")#lien
            ),
 tar_target(table_reseaucollab,
-           read.csv()#lien
-             ),           
+           read.csv("~/BAC/session 4/BIO500/travail/BIO500")#lien
+             )          
 )
   
