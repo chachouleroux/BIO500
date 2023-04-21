b8c84386cb71577dd16007b322deb5a8f18469c3
targ0401 <128487217+targ0401@users.noreply.github.com>
2023-04-21 17:08:40 -0400

    Delete _targets/ancienne commande directory

--- _targets/ancienne commande/adjacence.r
+++
@@ -1,9 +0,0 @@
-matrice_collab <- function(data) {
-  con <- dbConnect(SQLite(), dbname="~/BAC/session 4/BIO500/travail/BIO500/projet1.db")
-  matrice_sql.coll<- "
-  SELECT etudiant1, etudiant2 
-  FROM collaboration
-  ;"
-  data_matrice<- dbGetQuery(con, matrice_sql.coll)
-  adjacence_coll <- table(data_matrice)
-}
